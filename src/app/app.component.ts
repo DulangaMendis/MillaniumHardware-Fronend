69a12eaadc358ce4d91c4a98e68d79b39ef69fc4
DulangaMendis <dulangamendis82@gmail.com>
2024-12-04 16:34:02 +0530

    Create Routes Pages (item page , rental page , rental detail page)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CustomerComponent } from './routes/customer/customer.component';
+import { ItemComponent } from "./routes/item/item.component";
+import { RentalComponent } from "./routes/rental/rental.component";
+import { RentalDetailComponent } from "./routes/rental-detail/rental-detail.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CustomerComponent],
+  imports: [RouterOutlet,RentalDetailComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
