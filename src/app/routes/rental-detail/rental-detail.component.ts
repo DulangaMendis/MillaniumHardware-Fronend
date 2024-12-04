ca739d8a84122f81070cb91e75d9107acf07c3c2
DulangaMendis <dulangamendis82@gmail.com>
2024-12-04 15:49:08 +0530

    Create Customer page

---
+++ src/app/routes/rental-detail/rental-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rental-detail',
+  imports: [],
+  templateUrl: './rental-detail.component.html',
+  styleUrl: './rental-detail.component.css'
+})
+export class RentalDetailComponent {
+
+}
